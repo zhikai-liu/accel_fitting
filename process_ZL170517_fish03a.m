3a270c04a08cbdedd3513f9ca3e631a27731c48e
zhikai <zhikai@Bagnall-Lab-iMac.local>
2017-06-01 17:58:52 -0500

    test EPSC check function

--- process_ZL170517_fish03a.m
+++ process_ZL170517_fish03a.m
@@ -9,13 +9,15 @@ poi = {1:750000};
 %setappdata(F,'f_num',1);
 if_plot=0;
 [S_period,fit_model,accel_axis]= fit_accel(Data,si,f_name,poi,if_plot);
-amp_thre = 40; diff_gap = 240; diff_thre =-8;
+amp_thre = 6; diff_gap = 240; diff_thre =-8;
 [event_index, amps] = EPSC_detection(Data,si,amp_thre,diff_gap,diff_thre);
-F1 = figure;
-plot_accel_fit(Data,poi{1},fit_model{1},S_period{1},accel_axis,si);
-title(F1.Children(end),[f_name ' period ' num2str(1) ' of ' num2str(1)],'interpreter','none');
-F2 = figure;
-[cycle_index,amps_cycle] = plot_cycle_fit(Data,event_index,amps,poi{1},fit_model{1},S_period{1},si);
-F3 = figure;
-circ_plot(cycle_index,'pretty');
+save('test.mat','Data','si','event_index','amps')
+EPSC_check('test.mat',1)
+% F1 = figure;
+% plot_accel_fit(Data,poi{1},fit_model{1},S_period{1},accel_axis,si);
+% title(F1.Children(end),[f_name ' period ' num2str(1) ' of ' num2str(1)],'interpreter','none');
+% F2 = figure;
+% [cycle_index,amps_cycle] = plot_cycle_fit(Data,event_index,amps,poi{1},fit_model{1},S_period{1},si);
+% F3 = figure;
+% circ_plot(cycle_index,'pretty');
 
