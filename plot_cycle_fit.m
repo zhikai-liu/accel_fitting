e0d044a1a25bff00ebae119b302ec705759c2c12
zhikai <zhikai@Bagnall-Lab-iMac.local>
2017-06-07 18:13:56 -0500

    add CC plots

--- plot_cycle_fit.m
+++ plot_cycle_fit.m
@@ -2,10 +2,6 @@ function [cycle_index,amps] = plot_cycle_fit(Data,event_index,amps,poi,fit_model
         t_per_cycle = round(2*pi/fit_model.b1);
         cycle_num = round(length(S_period)/t_per_cycle);
         t_unit = 2*pi/t_per_cycle;
-        amps = amps(event_index>=S_period(1)&event_index<=S_period(end));
-        event_index = event_index(event_index>=S_period(1)&event_index<=S_period(end));
-        angle_index = fit_model.b1.*event_index+fit_model.c1;
-        cycle_index = mod(angle_index,2*pi);
         subplot(3,1,1);
             for i = 1:cycle_num
                 hold on;
@@ -25,7 +21,12 @@ function [cycle_index,amps] = plot_cycle_fit(Data,event_index,amps,poi,fit_model
             end
             xlim([0 360]);
             ylabel('g','Rotation',0);
-        subplot(3,1,3);
+        if ~isempty(event_index)
+            amps = amps(event_index>=S_period(1)&event_index<=S_period(end));
+            event_index = event_index(event_index>=S_period(1)&event_index<=S_period(end));
+            angle_index = fit_model.b1.*event_index+fit_model.c1;
+            cycle_index = mod(angle_index,2*pi);
+            subplot(3,1,3);
             for i = 1:length(event_index)
                 hold on;
                 scatter(cycle_index(i)*180/pi,amps(i),'r');
@@ -34,5 +35,6 @@ function [cycle_index,amps] = plot_cycle_fit(Data,event_index,amps,poi,fit_model
             xlim([0 360]);
             ylabel('pA','Rotation',0);
             xlabel('degree');
+        end
         samexaxis('abc','xmt','on','ytac','join','yld',1);  
 end
