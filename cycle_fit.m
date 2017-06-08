8a83da37fdaf0283315023b586c4ab38b60598a3
zhikai <zhikai@Bagnall-Lab-iMac.local>
2017-06-08 17:59:17 -0500

    add per_cycle_index

--- cycle_fit.m
+++ cycle_fit.m
@@ -1,23 +1,23 @@
-function [period_index,cycle_num] = cycle_fit(event_index,amps,fit_model,S_period)
+function [period_index,cycle_num,per_cycle_index] = cycle_fit(event_index,amps,fit_model,S_period)
     N = length(fit_model);
     period_index = struct();
     cycle_num = cell(1,N);
-    for i =1:length(fit_model)
+    per_cycle_index = struct();
+    for i =1:N
         t_per_cycle = round(2*pi/fit_model{i}.b1);
         cycle_num{i} = round(length(S_period{i})/t_per_cycle);
-%         per_cycle_index = cell(cycle_num,4);
-%         %t_unit = 2*pi/t_per_cycle;
-%%  This part is for finding the events index for each cycle of the sine wave
-%         for i = 1:cycle_num
-%             per_cycle_index{i,1} = event_index(event_index>=S_period(1)+(i-1)*t_per_cycle&event_index<S_period(1)+i*t_per_cycle);
-%             per_cycle_index{i,2} = fit_model.b1.*per_cycle_index{i,1}+fit_model.c1;
-%             per_cycle_index{i,3} = mod(per_cycle_index{i,2},2*pi);
-%             per_cycle_index{i,4} = amps(event_index>=S_period(1)+(i-1)*t_per_cycle&event_index<S_period(1)+i*t_per_cycle);
-%         end
+%         t_unit = 2*pi/t_per_cycle;
+%  This part is for finding the events index for each cycle of the sine wave
+        if ~isempty(event_index)
+            for j = 1:cycle_num{i}
+                per_cycle_index(i,j).event_index= event_index(event_index>=S_period{i}(1)+(j-1)*t_per_cycle&event_index<S_period{i}(1)+j*t_per_cycle);
+                per_cycle_index(i,j).angle_raw = fit_model{i}.b1.*per_cycle_index(i,j).event_index+fit_model{i}.c1;
+                per_cycle_index(i,j).phase = mod(per_cycle_index(i,j).angle_raw,2*pi);
+                per_cycle_index(i,j).amp = amps(event_index>=S_period{i}(1)+(j-1)*t_per_cycle&event_index<S_period{i}(1)+j*t_per_cycle);
+            end
 %%
         %For each period, we select only the EPSC events happned during that period.
         %first colunm is the event index of each EPSC
-        if ~isempty(event_index)
         period_index(i).event_index = event_index(event_index>=S_period{i}(1)&event_index<S_period{i}(end));
         %second column is the angle calculated based on the modelling,
         %which is the phase information
