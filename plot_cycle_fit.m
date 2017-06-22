dfb806a8e106e4fde8f4940a878f8c7244afe481
zhikai <zhikai@Bagnall-Lab-iMac.local>
2017-06-22 17:02:12 -0500

    plot 2 subplots while no amps

--- plot_cycle_fit.m
+++ plot_cycle_fit.m
@@ -2,7 +2,28 @@ function [cycle_index,amps] = plot_cycle_fit(Data,event_index,amps,poi,fit_model
         t_per_cycle = round(2*pi/fit_model.b1);
         cycle_num = round(length(S_period)/t_per_cycle);
         t_unit = 2*pi/t_per_cycle;
-        subplot(3,1,1);
+        fig_num =2;
+        if ~isempty(event_index)
+            fig_num =3;
+            amps = amps(event_index>=S_period(1)&event_index<=S_period(end));
+            event_index = event_index(event_index>=S_period(1)&event_index<=S_period(end));
+            angle_index = fit_model.b1.*event_index+fit_model.c1;
+            cycle_index = mod(angle_index,2*pi);
+            subplot(fig_num,1,3);
+            for i = 1:length(event_index)
+                hold on;
+                scatter(cycle_index(i)*180/pi,amps(i),'r');
+            end
+            hold off;
+            xlim([0 360]);
+            if strcmp(type,'EPSC+accel')||strcmp(type,'IPSC+accel')
+                ylabel('pA','Rotation',0);
+            elseif strcmp(type,'EPSP+accel')||strcmp(type,'IPSP+accel')
+                ylabel('mV','Rotation',0);
+            end
+            xlabel('degree');
+        end
+        subplot(fig_num,1,1);
             for i = 1:cycle_num
                 hold on;
                 plot([0:t_unit:2*pi].*180/pi,Data(S_period(1)+(i-1)*t_per_cycle:S_period(1)+i*t_per_cycle,1))
@@ -14,7 +35,7 @@ function [cycle_index,amps] = plot_cycle_fit(Data,event_index,amps,poi,fit_model
             elseif strcmp(type,'EPSP+accel')||strcmp(type,'IPSP+accel')
                 ylabel('mV','Rotation',0);
             end
-        subplot(3,1,2);
+        subplot(fig_num,1,2);
             color = {'r','g','b'};
             for i = 1:cycle_num
                 for j = 2:4
@@ -25,6 +46,7 @@ function [cycle_index,amps] = plot_cycle_fit(Data,event_index,amps,poi,fit_model
             end
             xlim([0 360]);
             ylabel('g','Rotation',0);
+<<<<<<< Updated upstream
         if ~isempty(event_index)
             amps = amps(event_index>=S_period(1)&event_index<=S_period(end));
             event_index = event_index(event_index>=S_period(1)&event_index<=S_period(end));
@@ -45,4 +67,8 @@ function [cycle_index,amps] = plot_cycle_fit(Data,event_index,amps,poi,fit_model
             xlabel('degree');
         end
         samexaxis('ytac','join');  
+=======
+        
+        samexaxis('abc','xmt','on','ytac','join','yld',1);  
+>>>>>>> Stashed changes
 end
