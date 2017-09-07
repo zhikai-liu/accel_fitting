9e608d2f6278c57b37dccd4ab65a952037ceac2e
Zhikai Liu <zhikai@wustl.edu>
2017-09-07 00:09:27 -0500

    add figure to raster

--- process_main.m
+++ process_main.m
@@ -1,18 +1,19 @@
 %process_view_files('ZL170517_fish03a');
-filename_h = 'ZL170518_fish03a';
+filename_h = 'ZL170901_fish04a';
 process_abf2mat(filename_h);
 process_event_detect(['EPSC_accel_' filename_h]);
-%process_event_detect(['EPSP_accel_' filename_h]);
+process_event_detect(['EPSP_accel_' filename_h]);
 process_fit(['EPSC_accel_' filename_h]);
-%process_fit(['IPSC_accel_' filename_h]);
+process_fit(['EPSP_accel_' filename_h]);
 %Plot fitting results for all
-%process_plot_fit(['EPSC_accel_' filename_h]);
-%process_plot_fit(['IPSC_accel_' filename_h]);
-%process_plot_each_cycle(filename_h);
+process_plot_fit(['EPSC_accel_' filename_h]);
+process_plot_fit(['EPSP_accel_' filename_h]);
+process_plot_each_cycle(['EPSC_accel_' filename_h]);
+process_plot_each_cycle(['EPSP_accel_' filename_h]);
 %close all
 %   Gather amplitudes and phase information together for further analysis
 %   .mat file will be generated (Amps.mat and trials.mat)
-process_cell_opening(['EPSC_accel_' filename_h]);
+%process_cell_opening(['EPSC_accel_' filename_h]);
 process_gather(['EPSC_accel_' filename_h]);
 % process_gather(['IPSC_accel_' filename_h]);
 %   Plot amp histogram for each recording file
