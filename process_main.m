afdb3608da041ead25cbcf4b1209f925b3121fd2
zhikai <zhikai@Zion.local>
2017-09-08 12:06:10 -0500

    Alignment of EPSP and EPSC

--- process_main.m
+++ process_main.m
@@ -1,4 +1,5 @@
 %process_view_files('ZL170517_fish03a');
+<<<<<<< HEAD
 filename_h = 'ZL170518_fish04a';
 process_abf2mat(filename_h);
 %process_event_detect(['EPSC_accel_' filename_h]);
@@ -9,12 +10,30 @@ process_fit(['EPSP_accel_' filename_h]);
 process_plot_fit(['EPSP_accel_' filename_h]);
 %process_plot_fit(['IPSC_accel_' filename_h]);
 %process_plot_each_cycle(filename_h);
+=======
+filename_h = 'ZL170901_fish04a';
+process_abf2mat(filename_h);
+process_event_detect(['EPSC_accel_' filename_h]);
+process_event_detect(['EPSP_accel_' filename_h]);
+process_fit(['EPSC_accel_' filename_h]);
+process_fit(['EPSP_accel_' filename_h]);
+%Plot fitting results for all
+process_plot_fit(['EPSC_accel_' filename_h]);
+process_plot_fit(['EPSP_accel_' filename_h]);
+process_plot_each_cycle(['EPSC_accel_' filename_h]);
+process_plot_each_cycle(['EPSP_accel_' filename_h]);
+>>>>>>> origin/EphysData
 %close all
 %   Gather amplitudes and phase information together for further analysis
 %   .mat file will be generated (Amps.mat and trials.mat)
 %process_cell_opening(['EPSC_accel_' filename_h]);
+<<<<<<< HEAD
 %process_gather(['EPSC_accel_' filename_h]);
 % process_gather(['IPSC_accel_' filename_h]);
+=======
+process_gather(['EPSC_accel_' filename_h]);
+process_gather(['EPSP_accel_' filename_h]);
+>>>>>>> origin/EphysData
 %   Plot amp histogram for each recording file
 % num_plot_per_figure = 4;
 % process_amps_hist_plot(filename_h,num_plot_per_figure);
---
+++ process_swim_fit.m
@@ -0,0 +1,6 @@
+function process_swim_fit(filename)
+    f_mat = dir([filename '*.mat']);
+    for i=1:length(f_mat)
+        
+    end
+end
