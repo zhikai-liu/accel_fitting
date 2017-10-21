47b47a290381321810c15b2177163ab7406aa59f
zhikai <zhikai@Zion.local>
2017-10-20 22:01:26 -0500

    Selected trials for each neuron

--- AmpBins_freq_dep.m
+++ AmpBins_freq_dep.m
@@ -3,19 +3,27 @@ S_amp=0.02;
 S_freq=[0.5 1 2 4 8];
 FNames=fieldnames(S);
 H=struct();
+T_select=struct();
 for i=1:length(FNames)
     ampbin_num=length(S.(FNames{i}));
     T=load(['Trials_EPSC_auto_accel_' FNames{i} '.mat']);
     amp_select=round([T.Trials.S_amp],2)==S_amp;
+    T_select(i).FName=FNames{i};
+    trials_all=1:length(T.Trials);
+    trials_A_select=trials_all(amp_select);
+    trials_select=[];
     A_select=T.Trials(amp_select);
     for k=1:length(S_freq)
         freq_select=round([A_select.S_freq],1)==S_freq(k);
         F_select=A_select(freq_select);
+        trials_F_select=trials_A_select(freq_select);
+        trials_select=[trials_select,trials_F_select];
         for h=1:length(F_select)
             H.(FNames{i}).(['S_freq_' num2str(k)])(h).S_freq=S_freq(k);
             H.(FNames{i}).(['S_freq_' num2str(k)])(h).period_index=F_select(h).period_index;
             H.(FNames{i}).(['S_freq_' num2str(k)])(h).S_cycle=F_select(h).S_cycle;
             H.(FNames{i}).(['S_freq_' num2str(k)])(h).AmpBins=S.(FNames{i});
+            H.(FNames{i}).(['S_freq_' num2str(k)])(h).trial_num=trials_F_select(h);
             amps=F_select(h).period_index.amp;
             phases=F_select(h).period_index.phase;
             events=F_select(h).period_index.event_index;
@@ -28,5 +36,7 @@ for i=1:length(FNames)
             end
         end
     end
+    T_select(i).trials_select=trials_select;
 end
 save('Trials_freq_dep.mat','-struct','H');
+save('Trials_freq_dep_t_select.mat','T_select');
--- event_interval_monitor.m
+++ event_interval_monitor.m
@@ -7,7 +7,7 @@ function event_interval_monitor(filename)
         Freq_names=fieldnames(S.(FNames{i}));
         %% plot each figure with one neuron's data, different ampbins in separate subplots
         AmpBin_Num=length(S.(FNames{i}).S_freq_1(1).AmpBins);
-        figure
+        figure('units','normal','position',[0.3,0,0.4,1])
         for h=1:AmpBin_Num
             %% Count intervals for each FName, sum all events together across frequencies
             interval_counts=[];
@@ -21,11 +21,17 @@ function event_interval_monitor(filename)
                 end
             end
             %% plot histogram of IFR, 1000 = 1ms interval
-            histogram(1./interval_counts,'BinWidth',100)
+            histogram(log10(interval_counts),50)
             amp_range=S.(FNames{i}).(Freq_names{j}).AmpBins(h).amp_range;
             text(0.4,0.8,[num2str(amp_range(1)) ' to ' num2str(amp_range(end)) ' pA' ],'units','Normalized')
+            xlim([-5 0])
+            xticks([-5 -4 -3 -2 -1 0])
+            xticklabels({'10^{-5}','10^{-4}','10^{-3}','10^{-2}','10^{-1}','1'})
+            xlabel('S')
         end
-        text(0.4,-0.3,FNames{i},'units','Normalized','interpreter','none')
+        F=gcf;
+        title(F.Children(end),FNames{i},'interpreter','none')
+        %text(0.4,-0.3,FNames{i},'units','Normalized','interpreter','none')
     end
 
 end
