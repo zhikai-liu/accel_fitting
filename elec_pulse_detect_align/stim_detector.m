f930bb304ad0a9421403ddd693fad746f24e1622
zhikai <zhikai@Zion.local>
2018-01-09 14:00:20 -0600

    stim_detector update

--- elec_pulse_detect_align/stim_detector.m
+++ elec_pulse_detect_align/stim_detector.m
@@ -6,7 +6,7 @@
 % Modified from EPSC_detection code
 
 function stim_detector(data,si)
-    v_d=data(:,1);
+    v_d=smooth(data(:,1));
     diff_v=diff(v_d)./si.*1e6;
     thre=4e6;
     cross_thre=diff_v>thre;
@@ -16,16 +16,35 @@ function stim_detector(data,si)
     figure;
     plot(v_d)
     hold on;
-    scatter(X,ones(length(X),1),'r*');
+    scatter(X,v_d(X),'r*');
     hold off;
     figure;
     hold on;
-    range_zero=X(1)-5:X(1)+20;
+    range_zero=X(1):X(1)+15;
+    trials=zeros(length(X),1001);
+    failures=zeros(length(X),1);
     for i=1:length(X)
-        range=X(i)-5:X(i)+20;
-        delay=finddelay(range_zero,range);
-        X_plot=X(i)-delay;
-        plot(-1000:1000,v_d(X_plot-1000:X_plot+1000))
+        range=X(i):X(i)+15;
+        delay=finddelay(v_d(range_zero)-mean(v_d(X(1)-50:X(1)-10)),v_d(range)-mean(v_d(X(i)-50:X(i)-10)));
+        X(i)=X(i)+delay;
+        trials(i,:)=v_d(X(i)-500:X(i)+500)-mean(v_d(X(i)-50:X(i)-10));
+        if trials(i,:)>-100
+            failures(i)=1;
+            plot(-500:500,trials(i,:),'r')
+        else
+            plot(-500:500,trials(i,:),'g')
+        end
+    end
+    hold off;
+    f_trials=trials(failures==1,:);
+    f_average=mean(f_trials,1);
+    colors=cell(length(X),1);
+    colors{failures==1}='r';
+    colors{failures==0}='g';
+    figure;
+    hold on;
+    for i=1:length(X)
+        plot(trials(i,:)-f_average,colors{i})
     end
     hold off;
 end
