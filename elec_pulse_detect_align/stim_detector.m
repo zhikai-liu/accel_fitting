86d062254539bbdad4e15504e19702b6fb4849cb
Zhikai Liu <zhikai@wustl.edu>
2018-01-10 14:51:19 -0600

    stim_detector update 2

--- elec_pulse_detect_align/stim_detector.m
+++ elec_pulse_detect_align/stim_detector.m
@@ -6,18 +6,21 @@
 % Modified from EPSC_detection code
 
 function stim_detector(data,si)
-    v_d=smooth(data(:,1));
+    v_d=smooth(data(:,1)); % data smoothing
+    %% calculate derivative of signal and find threshold crossing point
     diff_v=diff(v_d)./si.*1e6;
     thre=4e6;
     cross_thre=diff_v>thre;
-    sign_cross_thre=diff(cross_thre)==1;
+    sign_cross_thre=diff(cross_thre)==1;% find the first crossing point for an electrical pulse event
     X_=1:length(sign_cross_thre);
     X=X_(sign_cross_thre);
+    %% Plot data and detected beginning of an electrical pulse
     figure;
     plot(v_d)
     hold on;
     scatter(X,v_d(X),'r*');
     hold off;
+    %% Plot all events together after alignment
     figure;
     hold on;
     range_zero=X(1):X(1)+15;
@@ -37,14 +40,40 @@ function stim_detector(data,si)
     end
     hold off;
     f_trials=trials(failures==1,:);
-    f_average=mean(f_trials,1);
-    colors=cell(length(X),1);
-    colors{failures==1}='r';
-    colors{failures==0}='g';
+    f_average=mean(f_trials,1);  
+    %% Subtract failures to remove electrical artifact and only real signal remains
+    fail_EPSC=zeros(sum(failures==1),size(trials,2));
+    succ_EPSC=zeros(sum(failures~=1),size(trials,2));
+    latency=zeros(sum(failures~=1),1);
+    j=1;
+    k=1;
     figure;
     hold on;
     for i=1:length(X)
-        plot(trials(i,:)-f_average,colors{i})
+        if failures(i)==1
+            fail_EPSC(j,:)=trials(i,:)-f_average;
+            plot(fail_EPSC(j,:),'r')  
+            j=j+1;
+        else
+            succ_EPSC(k,:)=trials(i,:)-f_average;
+            plot(succ_EPSC(k,:),'g')
+            [~,EPSC_peak]=min(succ_EPSC(k,:));
+            latency(k)=EPSC_peak-501;
+            k=k+1;
+        end
     end
     hold off;
+    %% Align the EPSC signals based on their peaks
+    figure;
+    hold on;
+    for i=1:size(succ_EPSC,1)
+        x_range=1:1001;
+        plot(x_range-latency(i),succ_EPSC(i,:),'g')
+    end
+    hold off;
+    latency_ms=latency*si*1e-3;
+    figure;
+    histogram(latency_ms)
+    xlabel('ms')
+    STD=std(latency_ms)
 end
