e2c9c23335c5a88b8c902f26f34c1e7df2f11445
zhikai <zhikai@Zion.local>
2018-01-16 15:33:33 -0600

    add more inputs in varargin

--- EPSC_check.m
+++ EPSC_check.m
@@ -8,9 +8,16 @@
 % [event_index, amps] = EPSC_detection(Data,si,amp_thre,diff_gap,diff_thre);
 % save('test.mat','Data','si','event_index','amps')
 %% plot the data and detection results
-function EPSC_check(datafile,event_num)
+function EPSC_check(datafile,event_num,varargin)
 S = load(datafile);
-Data = S.Data; si = S.si; event_index = S.event_index;
+Data = S.Data; si = S.si;
+%%  if no extra input is recieved, use event_index; User can also specify to check only missing_events, for example.
+if isempty(varargin)
+    event_index = S.event_index;
+else 
+    event_index =S.(varargin{1});
+end
+%% Save Tags
 if isfield(S,'Tag')
     Tag = S.Tag;
 else
