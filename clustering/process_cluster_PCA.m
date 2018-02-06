f1148616ecf397a64ea375919685bd2e1854f901
zhikai <zhikai@Zion.local>
2018-02-05 20:54:18 -0600

    1st and 2nd derivative EPSC & add peak index for EPSC detection
    To separate single peak and overlapping EPSCs, der_index output is
    added for the EPSC_detection algorithm. Moreover, peak_index for each
    EPSC is added for better EPSC alignment

--- clustering/process_cluster_PCA.m
+++ clustering/process_cluster_PCA.m
@@ -1,33 +1,49 @@
 function event_raw = process_cluster_PCA(filename_h,range)
     f_mat = dir([filename_h '*.mat']);
+    rise_index=[];
     event_raw = [];
     Amps=[];
+    peak_index=[];
     if strcmp(range,'all')
         range=1:length(f_mat);
     end
     for i =range
         clearvars S
         S = load(f_mat(i).name);
+        SD=smooth(S.Data);
         for j = 1:length(S.event_index)
             index = S.event_index(j);
-            if abs(index-16700)>200&& abs(index-26700)>200 && index-50>1 &&index+100<length(S.Data)
-            event_trace=smooth(S.Data(index-50:index+100,1));
-            event_trace=event_trace-mean(event_trace(40:50));
-            event_raw = [event_raw,event_trace];
-            Amps=[Amps,S.amps(j)];
+            if abs(index-16700)>200&& abs(index-26700)>200 && index-50>1 &&index+100<length(SD)              
+                rise_index=[rise_index,index];
+                Amps=[Amps,S.amps(j)];
+                peak_index=[peak_index,S.event_peak(j)];
+                event_clips=SD(index-50:index+100,1);
+                event_clips=event_clips-mean(event_clips(40:50));
+
+%                 event_clips=SD(S.event_peak(j)-50:S.event_peak(j)+100,1);
+%                 event_clips=event_clips-mean(SD(index-5:index));
+                event_raw = [event_raw,event_clips];
             end
         end
     end
-    [coeff,score,latent] = pca(event_raw(50:120,:)');
-    clust_num = 10;
-    clust_index = kmeans(event_raw(50:120,:)',clust_num);
+    cluster_parts=event_raw(50:100,:);
+    [coeff,score,latent,~,explained] = pca(cluster_parts');
+    %clust_num = 10;
+    %clust_index = kmeans(event_raw(50:120,:)',clust_num);
+    opts.isocut_threshold=1;
+    %opts.prevent_merge=true;
+    clust_index=isosplit5(cluster_parts,opts);
     %Color = {'r','g','b','k','m','c'};
-    map=[1,0,0;0,1,0;0,0,1;0,0,0;1,0,1;0,1,1];
-    map=[map;map./2];
+    %map=[1,0,0;0,1,0;0,0,1;0,0,0;1,0,1;0,1,1];
+    %map=[map;map./2];
+    %ms_view_clusters(score',clust_index);
+    clust_num=max(clust_index);
+    map=colormap(jet(clust_num));
     figure;
     hold on;
-    for i = 1:length(score)
-    scatter3(score(i,1),score(i,2),score(i,3),'MarkerEdgeColor',map(clust_index(i),:));
+    for i = 1:clust_num
+        inds=find(clust_index==i);
+        scatter3(score(inds,1),score(inds,2),score(inds,3),'MarkerEdgeColor',map(i,:));
     end
     figure;
     Amps_clust=cell(1,clust_num);
@@ -38,7 +54,6 @@ function event_raw = process_cluster_PCA(filename_h,range)
     nhist(Amps_clust,'numbers','smooth','binfactor',20,'color','colormap')
     figure;
     for i = 1:length(event_raw)
-       
         plot(event_raw(:,i),'color',map(clust_index(i),:))
         hold on;
     end
@@ -52,4 +67,5 @@ function event_raw = process_cluster_PCA(filename_h,range)
             end
         end
     end
+    samexaxis('ytac','join','box','off');
 end
--- detect_EPSC/EPSC_detection.m
+++ detect_EPSC/EPSC_detection.m
@@ -1,8 +1,8 @@
-function [event_index, amps] = EPSC_detection(W,si,amp_thre,if_2der,diff_gap,diff_thre,event_duration)
+function [event_index,event_peak, amps] = EPSC_detection(W,si,amp_thre,if_2der,diff_gap,diff_thre,event_duration)
 %% calculate the difference with 240us as "1st derivative" to detect event
 diff_gap = diff_gap/si;
 event_duration =event_duration/si;
-data_s = smooth(W);  %smooth the data 
+data_s = smooth(W);  %smooth the data
 diff_ = data_s(1+diff_gap:end)-data_s(1:end-diff_gap)-diff_thre; % caculate the difference between 240us
 crossing_ = diff_(1:end-1).*diff_(2:end)<0;% find the crossing for -8pA
 r_index = find(crossing_.*(diff_(1:end-1)>0)); % find the on-rise crossing point
@@ -18,32 +18,46 @@ end
 
 %% calculate 2nd derivative and find zero crossing between diff_crossing pair
 if if_2der
-extra_index= [];
-for i = 1:length(r_index)
-    der_ = diff(diff_(r_index(i):f_index(i))); %calculate derivative
-    zero_ = der_(1:end-1).*der_(2:end)<=0; % find zero crossing
-    infle_ = find(zero_.*(der_(1:end-1)>0)); %find inflection point
-    if ~isempty(infle_)
-    extra_index = [extra_index;infle_+r_index(i)]; % find extra index to add for overlapped EPSCs
+    extra_index= [];
+    exist_der2=zeros(length(r_index),1);
+    for i = 1:length(r_index)
+        der_ = diff(diff_(r_index(i):f_index(i))); %calculate derivative
+        zero_ = der_(1:end-1).*der_(2:end)<=0; % find zero crossing
+        infle_ = find(zero_.*(der_(1:end-1)>0)); %find inflection point
+        if ~isempty(infle_)
+            extra_index = [extra_index;infle_+r_index(i)]; % find extra index to add for overlapped EPSCs
+            exist_der2(i)=1;
+        end
     end
+    
+    der2_index = [r_index(exist_der2==1);extra_index];
+    regular_index=r_index(exist_der2==0);
+    all_index=[regular_index;der2_index];
+    all_index=[all_index,ones(length(all_index),1)];
+    all_index(length(regular_index)+1:end,2)=2;
+    all_index = sortrows(all_index);
 end
 %% calculate amplitude for each events detected
-raw_index = sort([r_index;extra_index]);
-else
-    raw_index = sort(r_index);
-end
+raw_index=all_index(:,1);
 raw_l = length(raw_index);
 amp_raw = zeros(raw_l,1);
+peak_index = zeros(raw_l,1);
 for i = 1:raw_l
     if i == raw_l
-        duration = min(event_duration,length(data_s)-raw_index(i)); % the last EPSC duration is 640us
+        duration = min(event_duration,length(data_s)-raw_index(i)); % when it is the last EPSC, make sure duration of EPSC didn't go beyond the end of the whole trace
     else
-    duration = min(raw_index(i+1)-raw_index(i),32);%duration of EPSC is the smaller one of either 640us or before next EPSC comes
+        duration = min(raw_index(i+1)-raw_index(i),32);% Duration of EPSC is the smaller one of either 640us or before next EPSC comes
     end
-    amp_raw(i) = max(abs(data_s(raw_index(i):raw_index(i)+duration)-data_s(raw_index(i))));
+    [peak_value,peak_index(i)] = min(data_s(raw_index(i):raw_index(i)+duration));
+    amp_raw(i)=data_s(raw_index(i))-peak_value;
+    peak_index(i)=raw_index(i)+peak_index(i)-1;
+    % Calculate the amplitude of each EPSC, here the algorithm is:
+    % max value within the trace - value at the index point
 end
 
 %% set amplitudes threshold for EPSC detection
 amps = amp_raw(amp_raw>amp_thre);
-event_index = raw_index(amp_raw>amp_thre);
+event_index = all_index(amp_raw>amp_thre,1);
+der_index=all_index(amp_raw>amp_thre,2);
+event_peak=peak_index(amp_raw>amp_thre);
 end
