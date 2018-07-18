0860ba659c89972c3741c5e65aa67c8391ee8bf8
zhikai <zhikai@Zion.local>
2018-07-18 13:12:40 -0500

    add gpuArray option

--- Event_detection/process_fista_2tem_detect.m
+++ Event_detection/process_fista_2tem_detect.m
@@ -12,17 +12,20 @@ for i =1:length(f_mat)
     alpha=EPSC_w1'*EPSC_w1/(EPSC_w1'*EPSC_w2);
     fista.template2=EPSC_w1-alpha.*EPSC_w2;
     opts.backtracking=true;
-    opts.verbose=false;
+    opts.verbose=true;
     opts.lambda1=rms(signal).*max(abs(fista.template1)).*norminv(0.99);
     opts.lambda2=rms(signal).*max(abs(fista.template2)).*norminv(0.99);
     opts.pos=false;
     Xinit=[];
     %% Main fista algorithm
-    tic
-    [fista.X1,fista.X2,fista.cost_iter] = fista_lasso_backtracking_2tems(signal, fista.template1,fista.template2, Xinit,Xinit, opts);
+    if parallel.gpu.GPUDevice.isAvailable
+        [fista.X1,fista.X2,fista.cost_iter] = fista_lasso_backtracking_2tems(gpuArray(signal),...
+            gpuArray(fista.template1),gpuArray(fista.template2), gpuArray(Xinit),gpuArray(Xinit), opts);
+    else
+        [fista.X1,fista.X2,fista.cost_iter] = fista_lasso_backtracking_2tems(signal, fista.template1,fista.template2, Xinit,Xinit, opts);
+    end
     [fista.X1_max,fista.recon_integral,fista.chemical]=fista_local_maxima(signal,fista.X1,fista.X2,fista.template1,fista.template2);
     fista_autocorrelogram(fista.X1_max,fista.recon_integral,fista.chemical)
     save(f_mat(i).name,'fista','-append');
-    toc
 end
 end
